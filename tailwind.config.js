2aa0749608432125c2b96acb5b6c8d4769dc286a
kotagisourabh81 <kotagisourabh81@gmail.com>
2023-07-01 12:28:53 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
